2ddd8d3cc017961b351e9a6cfadd1a8a95edda35
Amaka <amakajoseph48@gmail.com>
2025-10-15 17:31:29 +0100

    update js

--- brands.js
+++ brands.js
@@ -33,3 +33,20 @@ function startNavbarNotesRotation() {
 
 // Start rotation on page load
 startNavbarNotesRotation()
+
+let searchInput = document.querySelector('.search input')
+searchInput.addEventListener('keyup', filterItem)
+
+
+function filterItem() {
+    let searched = searchInput.value.toUpperCase()
+    let brandName = document.querySelectorAll('.brand')
+    for(let i = 0; i < brandName.length; i++) {
+        let name = brandName[i].textContent || brandName[i].innerText
+        if (name.toUpperCase().indexOf(searched) > -1 ){
+            brandName[i].style.display = ''
+        } else {
+            brandName[i].style.display = 'none'
+        }
+    }
+}
